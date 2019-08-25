7200ae728b0fe0bf7d1ceff1f2b4656112753a1d
ta-chibana <tachibana.gmc@gmail.com>
2019-08-25 13:19:02 +0900

    Change OpenStruct to double

--- spec/aasm_to_plantuml/aasm_node_finder_spec.rb
+++ spec/aasm_to_plantuml/aasm_node_finder_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe AasmToPlantuml::AasmNodeFinder do
   describe '#calling_aasm_node?' do
-    let(:node) { OpenStruct.new(type: :FCALL, children: [:aasm, []]) }
+    let(:node) { double(type: :FCALL, children: [:aasm, []]) }
     let(:subject) { described_class.new(double).calling_aasm_node?(node) }
 
     it { is_expected.to be true }
--- spec/aasm_to_plantuml/depth_first_search_spec.rb
+++ spec/aasm_to_plantuml/depth_first_search_spec.rb
@@ -2,14 +2,14 @@
 
 RSpec.describe AasmToPlantuml::DepthFirstSearch do
   describe '#find_by' do
-    let(:node1) { OpenStruct.new(value: 1, children: [node2, node3]) }
-    let(:node2) { OpenStruct.new(value: 2, children: [node4]) }
-    let(:node3) { OpenStruct.new(value: 3, children: [node5, node6]) }
-    let(:node4) { OpenStruct.new(value: 4, children: []) }
-    let(:node5) { OpenStruct.new(value: 5, children: [node7, node8]) }
-    let(:node6) { OpenStruct.new(value: 6, children: []) }
-    let(:node7) { OpenStruct.new(value: 7, children: []) }
-    let(:node8) { OpenStruct.new(value: 8, children: []) }
+    let(:node1) { double(value: 1, children: [node2, node3]) }
+    let(:node2) { double(value: 2, children: [node4]) }
+    let(:node3) { double(value: 3, children: [node5, node6]) }
+    let(:node4) { double(value: 4, children: []) }
+    let(:node5) { double(value: 5, children: [node7, node8]) }
+    let(:node6) { double(value: 6, children: []) }
+    let(:node7) { double(value: 7, children: []) }
+    let(:node8) { double(value: 8, children: []) }
 
     subject { described_class.new(node1).find_by(value) }
 
